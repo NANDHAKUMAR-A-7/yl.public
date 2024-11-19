694750fedebcb35e710cd59f3681143857452f62
Nandhakumar A <fdemo1177@gmail.com>
2024-11-19 20:21:48 +0530

    js file is ready

--- main.js
+++ main.js
@@ -10,4 +10,7 @@ const message = document.getElementById("message");
     console.log(event.target);
     sendbtn.style.display = "block";
 });
-   
+    sendbtn.addEventListener("click" ,function(event){
+        alert("Get Replay As Soon As Once Our Team Receive Your Message ! ...Thank You For Contacting Us."           
+        )
+    });
